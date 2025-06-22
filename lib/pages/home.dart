9fd224210d54828e1f1adc4f3226799642e645ef
Muhammad Ghazali <137089802+kaedehara2@users.noreply.github.com>
2025-06-22 21:45:52 +0700

    Penambahan 2 icon untuk search dan filter menggunakan SVG

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -5,29 +5,110 @@ class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Breakfast',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 17,
-            fontWeight: FontWeight.bold
-          ),
-          ),
-          backgroundColor: Colors.white,
-          elevation: 0.0,
-        centerTitle: true,
-        leading: Container(
-          margin: EdgeInsets.all(10),
-          child: SvgPicture.asset('assets/icons/icon1.svg'),
-          decoration: BoxDecoration(
-            color: Color(0xffF7F8F8),
-            borderRadius: BorderRadius.circular(10)
-          ),
+      appBar: appBar(),
+      body: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.only(top: 40, left: 20, right: 20),
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0xff101617).withOpacity(0.11),
+                  blurRadius: 40,
+                  spreadRadius: 0.0 
+                )
+              ]
+            ),
+            child: TextField(
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: EdgeInsets.all(15),
+                prefixIcon: Padding(
+                  padding: const EdgeInsets.all(12),
+                  child: SvgPicture.asset('assets/icons/se.svg'),
+                ),
+                suffixIcon: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    VerticalDivider(
+                      color: Color(0xffD0DADA),
+                      thickness: 0.1,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SvgPicture.asset('assets/icons/Filters.svg'),
+                    ),
+                  ],
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(15),
+                  borderSide: BorderSide.none
+                )
+              ),
+            ),
+          )
+        ], 
+      ),
+    );
+  }
+
+  AppBar appBar() {
+    return AppBar(
+      title: Text(
+        'Breakfast',
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 17,
+          fontWeight: FontWeight.bold
+        ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0.0,
+      centerTitle: true,
+      leading: GestureDetector(
+        onTap: () {
+          
+        },
+      
+      child: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        child: SvgPicture.asset(
+          'assets/icons/icon1.svg',
+             height: 20,
+             width: 20,),
+     
+        decoration: BoxDecoration(
+          color: Color(0xffF7F8F8),
+          borderRadius: BorderRadius.circular(10)
         ),
       ),
+      ),
+      actions: [
+        GestureDetector(
+          onTap: () {
+
+          },
+        child: Container(
+        margin: EdgeInsets.all(10),
+        alignment: Alignment.center,
+        width: 37,
+        child: SvgPicture.asset(
+          'assets/icons/icon2.svg',
+           height: 5,
+           width: 5,),
+       
+        decoration: BoxDecoration(
+          color: Color(0xffF7F8F8),
+          borderRadius: BorderRadius.circular(10)
+        ),
+      ),
+        ),
+      ],
     );
   }
 }
