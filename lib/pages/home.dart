6bda6246b5a67d81cc04e60871307b79a82cd87e
Muhammad Ghazali <137089802+kaedehara2@users.noreply.github.com>
2025-07-07 13:57:19 +0700

    menu rekomendasi diet

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:vitnezg/models/category_model.dart';
+import 'package:vitnezg/models/diet_model.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -11,15 +12,16 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<CategoryModel> categories = [];
+  List<DietModel> diets = [];
 
-  void _getCategories() {
+  void _getInitialInfo() {
     categories = CategoryModel.getCategories();
+    diets = DietModel.getDiets();
   }
-
   
   @override
   Widget build(BuildContext context) {
-    _getCategories();
+    _getInitialInfo();
     return Scaffold(
       appBar: appBar(),
       backgroundColor: Colors.white,
@@ -28,71 +30,83 @@ class _HomePageState extends State<HomePage> {
         children: [
           _searchField(),
           SizedBox(height: 40,),
+          _categoriesSection(),
+          SizedBox(height: 40,),
           Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 20),
-              
-              child: Text(
-                'Category',
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 18,
-                  fontWeight: FontWeight.w600
-                ),
-              )
-              ),
-              SizedBox(height: 15,),
-              Container(
-                height: 120,
-                //color: Colors.greenAccent,
-                child: ListView.separated(
-                  itemCount: categories.length,
-                  scrollDirection: Axis.horizontal,
-                  padding: EdgeInsets.only(
-                    left: 20,
-                    right: 20
-                  ),
-                  separatorBuilder: (context, index) => SizedBox(width: 25,), //untuk scroll kanan kiri
-                  itemBuilder: (context, index) {
-                    return Container(
-                      width: 100,
-                      decoration: BoxDecoration(
-                        color: categories[index].boxColor.withOpacity(0.3),
-                        borderRadius: BorderRadius.circular(16)
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: [
-                          Container(
-                            width: 50,
-                            height: 50,
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              shape: BoxShape.circle
-                            ),
-                            child: SvgPicture.asset(categories[index].iconPath),
-                          ),
-                          Text(
-                            categories[index].name,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w400,
-                              color: Colors.black,
-                              fontSize: 14
-                            ),
-                          )
-                        ],
-                      ),
-                    );
-                  }
-              )
+              Text(
+                'Recommendation\nfor Diet'
               )
             ],
           )
         ], 
       ),
     );
+  }
+
+  Column _categoriesSection() {
+    return Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(left: 20),
+            
+            child: Text(
+              'Category',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.w600
+              ),
+            )
+            ),
+            SizedBox(height: 15,),
+            Container(
+              height: 120,
+              //color: Colors.greenAccent,
+              child: ListView.separated(
+                itemCount: categories.length,
+                scrollDirection: Axis.horizontal,
+                padding: EdgeInsets.only(
+                  left: 20,
+                  right: 20
+                ),
+                separatorBuilder: (context, index) => SizedBox(width: 25,), //untuk scroll kanan kiri
+                itemBuilder: (context, index) {
+                  return Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: categories[index].boxColor.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(16)
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Container(
+                          width: 50,
+                          height: 50,
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            shape: BoxShape.circle
+                          ),
+                          child: SvgPicture.asset(categories[index].iconPath),
+                        ),
+                        Text(
+                          categories[index].name,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w400,
+                            color: Colors.black,
+                            fontSize: 14
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                }
+            )
+            )
+          ],
+        );
   }
 
   Container _searchField() {
